fb90ad044007408f474b7da395d8f7925ed42281
Emam546 <emam54637@gmail.com>
2023-03-13 04:57:13 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import "./App.css";
+
+function App() {
+
+    return <></>;
+}
+
+export default App;
--- src/declarations/major/EnvVars.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable node/no-process-env */
-
-import { NodeEnvs } from "../enums";
-
-export default {
-  nodeEnv: (process.env.NODE_ENV ?? NodeEnvs.Dev) as NodeEnvs,
-  port: (process.env.PORT ?? 0),
-  API_KEY:(process.env.API_KEY ?? ""),
-} as const;
